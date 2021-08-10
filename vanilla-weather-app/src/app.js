510a9d02f5cf2f85f2cd3fcbe22265119413d025
Natalia Orsi <naty.m.orsi@gmail.com>
2021-08-10 09:01:37 +0200

    Added first JS code

---
+++ vanilla-weather-app/src/app.js
@@ -0,0 +1,17 @@
+function displayTemperature(response) {
+    let temperatureElement = document.querySelector("#temperature");
+    let cityElement = document.querySelector("#city");
+    let descriptionElement = document.querySelector("#description");
+    let humidityElement = document.querySelector("#humidity");
+    let windElement = document.querySelector("#wind");
+    temperatureElement.innerHTML = Math.round(response.data.main.temp);
+    cityElement.innerHTML = response.data.name;
+    descriptionElement.innerHTML =  response.data.weather[0].description;
+    humidityElement.innerHTML = response.data.main.humidity;
+    windElement.innerHTML = Math.round(response.data.wind.speed);
+}
+
+let apiKey = "3b2c6af1778711e6c3af06800ecbd9b8";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=New York&appid=${apiKey}&units=metric`;
+
+axios.get(apiUrl).then(displayTemperature);
