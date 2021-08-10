45d3d055cdd63b4fdaf2b9e7479fc14cd2a5ee12
Natalia Orsi <naty.m.orsi@gmail.com>
2021-08-10 09:25:32 +0200

    Added relevant icon

--- vanilla-weather-app/src/app.js
+++ vanilla-weather-app/src/app.js
@@ -4,11 +4,18 @@ function displayTemperature(response) {
     let descriptionElement = document.querySelector("#description");
     let humidityElement = document.querySelector("#humidity");
     let windElement = document.querySelector("#wind");
+    let iconElement = document.querySelector("#icon")
+
     temperatureElement.innerHTML = Math.round(response.data.main.temp);
     cityElement.innerHTML = response.data.name;
     descriptionElement.innerHTML =  response.data.weather[0].description;
     humidityElement.innerHTML = response.data.main.humidity;
     windElement.innerHTML = Math.round(response.data.wind.speed);
+    iconElement.setAttribute(
+      "src",
+      `http://openweathermap.org/img/wn/${respnse.data.weather[0].icon}@2x.png`
+    );
+    iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 
 let apiKey = "3b2c6af1778711e6c3af06800ecbd9b8";
