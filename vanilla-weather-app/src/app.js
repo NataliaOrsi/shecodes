9caba984ed48e8e2b1a144b25c6f39d2e24b79e7
Natalia Orsi <naty.m.orsi@gmail.com>
2021-08-11 10:38:41 +0200

    Added the search engine in JS

--- vanilla-weather-app/src/app.js
+++ vanilla-weather-app/src/app.js
@@ -18,7 +18,19 @@ function displayTemperature(response) {
     iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 
-let apiKey = "3b2c6af1778711e6c3af06800ecbd9b8";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=New York&appid=${apiKey}&units=metric`;
+function search(city) {
+  let apiKey = "3b2c6af1778711e6c3af06800ecbd9b8";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayTemperature);
+}
 
-axios.get(apiUrl).then(displayTemperature);
+function handleSubmit(event) {
+  event.preventDefault();
+  let cityInputElement = document.querySelector("#city-input");
+  search(cityInputElement.value);
+}
+
+search("New York");
+
+let searchForm = document.querySelector("#search-form");
+searchForm.addEventListener("submit", handleSubmit);
