75cd1169b72c18d10a2cdb306d4304ade7ef7395
Natalia Orsi <naty.m.orsi@gmail.com>
2021-08-13 11:13:47 +0200

    Added getForecast function

--- vanilla-weather-app/src/app.js
+++ vanilla-weather-app/src/app.js
@@ -21,7 +21,7 @@ function formatDate(timestamp) {
   return `${day} ${hours}:${minutes}`;
 }
 
-function displayForecast () {
+function displayForecast (response) {
    let forecastElement = document.querySelector("#forecast");
 
    let forecastHTML = `<div class="row">`;
@@ -49,7 +49,14 @@ function displayForecast () {
    })
   forecast = forecastHTML + `<div>`;
   forecastElement.innerHTML = forecastHTML;
+}
 
+function getForecast(coordinates) {
+  console.log(coordinates);
+  let apiKey = "3b2c6af1778711e6c3af06800ecbd9b8";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}`;
+  console.log(apiUrl);
+  axios.get(apiUrl).then(displayForecast);
 }
 
 function displayTemperature(response) {
@@ -73,6 +80,8 @@ function displayTemperature(response) {
     `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
   iconElement.setAttribute("alt", response.data.weather[0].description);
+
+  getForecast(response.data.coord);
 }
 
 function search(city) {
@@ -117,4 +126,3 @@ let celsiusLink = document.querySelector("#celsius-link");
 celsiusLink.addEventListener("click", displayCelsiusTemperature);
 
 search("New York");
-displayForecast();
